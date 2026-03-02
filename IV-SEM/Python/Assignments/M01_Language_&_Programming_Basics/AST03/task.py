a00e5a8937cecda4c0780fe186ea0bcfe143bedd
Nanditha <nandithareddynarra@gmail.com>
2026-03-02 14:46:44 +0530

    msg

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -12,3 +12,8 @@ def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
         status = "fail"
 
     return f"Average grade: {avg_str}, Status: {status}"
+
+if __name__ == '__main__':
+    name = input()
+    n1,n2,n3 = list(map(int,input().split()))
+    print(Student_Grade_System(name,n1,n2,n3))
