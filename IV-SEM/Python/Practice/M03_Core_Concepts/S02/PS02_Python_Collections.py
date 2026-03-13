80e74f70207d0169b1a2c0dec16911910cefadf5
Nanditha <nandithareddynarra@gmail.com>
2026-03-13 11:52:53 +0530

    msg

---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collections.py
@@ -0,0 +1,16 @@
+'''1)Creating a list'''
+a= [1,2,3,4,5,6]
+print(a)
+b = list((1,2,3,4,5))
+print(b)
+'''2)Accessing list index elements'''
+print(a[1])
+print(a[-1])
+'''3)Creating list with repeated elements'''
+a = [1,2,3,5]
+print(a*2)
+'''4)Adding elements to a list'''
+a = [1,2,3]
+a.append(50)
+a.insert(1,20)
+''''''
